357923a880f4101b93fb254c89e03d072b97026c
riwepo <riwepo.au@gmail.com>
2025-08-19 08:30:35 +1000

    writing full ted talk

--- src/ted_scrape/odt_writer.clj
+++ src/ted_scrape/odt_writer.clj
@@ -1,36 +1,56 @@
 (ns ted-scrape.odt-writer
+  (:require [clojure.edn :as edn])
   (:import [org.odftoolkit.odfdom.doc OdfTextDocument]
            [org.odftoolkit.odfdom.dom.element.text TextHElement TextPElement]))
 
-(defn write-heading2 [doc heading-text]
+(def heading-formats {:h1 {:style-name "Heading_20_1" :outline-level 1}
+                      :h2 {:style-name "Heading_20_2" :outline-level 2}
+                      :h3 {:style-name "Heading_20_3" :outline-level 3}})
+
+(def style-names {:body "Text_20_body"})
+
+(defn write-heading [doc format text]
   (let [dom (.getContentDom doc)
         heading (TextHElement. dom)]
-    (.setTextContent heading heading-text)
-    (.setAttribute heading "text:style-name" "Heading_20_2")
+    (.setTextContent heading text)
+    (.setTextStyleNameAttribute heading (:style-name format))
+    (.setTextOutlineLevelAttribute heading (int (:outline-level format)))
     (.appendChild (.getContentRoot doc) heading)
-    doc))                                                   ; return doc for chaining
+    doc))
 
-(defn write-para [doc para-text]
+(defn write-para [doc style-name text]
   (let [dom (.getContentDom doc)
         para (TextPElement. dom)]
-    (.setTextContent para para-text)
-    (.setAttribute para "text:style-name" "Text_20_body")
+    (.setTextContent para text)
+    (.setTextStyleNameAttribute para style-name)
     (.appendChild (.getContentRoot doc) para)
-    doc))                                                   ; return doc for chaining
+    doc))
 
-(defn richo2 []
-  (-> (OdfTextDocument/newTextDocument)
-      (write-heading2 "Chapter 1")
-      (write-para "")
-      (write-para "")
-      (write-para "This is normal body text.")
-      (write-para "")
-      (write-para "With somme line breaks")
-      (.save "data/test.odt")))
+(defn write-ted-talk [ted-talk]
+  (let [doc (-> (OdfTextDocument/newTextDocument)
+                (write-heading (:h1 heading-formats) (str "TED Talk: " (:title ted-talk)))
+                (write-para (:body style-names) "")
+                (write-heading (:h3 heading-formats) (str "Speaker: " (:speaker ted-talk)))
+                (write-para (:body style-names) "")
+                (write-heading (:h3 heading-formats) "Introduction")
+                (write-para (:body style-names) (:description ted-talk))
+                (write-para (:body style-names) "")
+                (write-heading (:h3 heading-formats) "Transcript"))]
+    (reduce (fn [d para]
+              (write-para d (:body style-names) (:timestamp para))
+              (write-para d (:body style-names) (:content para))
+              (write-para d (:body style-names) ""))
+            doc
+            (:transcript ted-talk))))
+
+(defn richo []
+  (let [raw-str (slurp "data/ted-talk.edn")
+        ted-talk (edn/read-string raw-str)
+        doc (write-ted-talk ted-talk)]
+    (.save doc "data/test.odt")))
 
 (comment
-  (richo2)
-  (create-doc)
+  (richo)
   (nop))
 
 
