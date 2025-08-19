49e28a70f099ab51a1d211ef63041ad0c1534ac1
riwepo <riwepo.au@gmail.com>
2025-08-20 09:48:15 +1000

    check uri is reachable  return value from main logic

--- src/ted_scrape/core.clj
+++ src/ted_scrape/core.clj
@@ -1,4 +1,20 @@
-(ns ted-scrape.core)
-(defn run [options]
-  (println "running program with these args" options)
-  true)
+(ns ted-scrape.core
+  (:require [clj-http.client :as http]))
+
+(defn reachable-url?
+  "Returns true if the URL responds with a 2xx or 3xx status code."
+  [^String url]
+  (try
+    (let [resp (http/head url {:throw-exceptions false})]
+      (<= 200 (:status resp) 399))
+    (catch Exception _ false)))
+
+
+(defn run [{:keys [url output-dir]}]
+  (println "running program with these args" url output-dir)
+  (if
+    (not (reachable-url? url))
+      {:ok? false :message "url not reachable"}
+      {:ok? true :message "finished"}))
+
+
