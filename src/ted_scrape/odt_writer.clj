d4ca1d54be08b16f390518d38e5993f8ec32426d
riwepo <riwepo.au@gmail.com>
2025-08-19 10:57:52 +1000

    achieved no page breaks inside paragraphs

--- src/ted_scrape/odt_writer.clj
+++ src/ted_scrape/odt_writer.clj
@@ -1,13 +1,54 @@
 (ns ted-scrape.odt-writer
   (:require [clojure.edn :as edn])
   (:import [org.odftoolkit.odfdom.doc OdfTextDocument]
+           [org.odftoolkit.odfdom.dom.element.style
+            StyleParagraphPropertiesElement]
            [org.odftoolkit.odfdom.dom.element.text TextHElement TextLineBreakElement TextPElement]))
 
 (def heading-formats {:h1 {:style-name "Heading_20_1" :outline-level 1}
                       :h2 {:style-name "Heading_20_2" :outline-level 2}
                       :h3 {:style-name "Heading_20_3" :outline-level 3}})
 
-(def style-names {:body "Text_20_body"})
+(def paragraph-style "Text_20_body")
+
+(defn modify-paragraph-style [doc style-name]
+  "we modify an existing paragraph style
+  so that we will not get page breaks
+  in the middle of a paragraph"
+  (let [styles (.getStylesDom doc)
+        all-styles (.getElementsByTagName styles "style:style")
+        style (first
+                (filter #(= (.getAttribute % "style:name") style-name)
+                        (map #(.item all-styles %) (range (.getLength all-styles)))))]
+
+    (if (nil? style)
+      (do
+        (println (str "Style not found: " style-name))
+        doc)
+
+      (let [props-node-list (.getElementsByTagName style "style:paragraph-properties")
+            props (if (pos? (.getLength props-node-list))
+                    (.item props-node-list 0)
+                    (.newOdfElement styles StyleParagraphPropertiesElement))]
+
+        ;; Set fo:keep-together
+        (.setAttributeNS props
+                         "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"
+                         "fo:keep-together"
+                         "always")
+
+        ;; Set fo:break-inside
+        (.setAttributeNS props
+                         "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0"
+                         "fo:break-inside"
+                         "avoid")
+
+        ;; Attach props if not already present
+        (when (zero? (.getLength props-node-list))
+          (.appendChild style props))
+
+        doc))))
+
 
 (defn write-heading [doc format text]
   (let [dom (.getContentDom doc)
@@ -22,7 +63,7 @@
   (let [dom (.getContentDom doc)
         para (TextPElement. dom)
         lines (clojure.string/split text #"\n")]
-    (.setTextStyleNameAttribute para style-name)
+    (.setStyleName para style-name)
     (doseq [[i line] (map-indexed vector lines)]
       (.appendChild para (.createTextNode dom line))
       (when (< i (dec (count lines)))
@@ -32,17 +73,18 @@
 
 (defn write-ted-talk [ted-talk]
   (let [doc (-> (OdfTextDocument/newTextDocument)
+                (modify-paragraph-style paragraph-style)
                 (write-heading (:h1 heading-formats) (str "TED Talk: " (:title ted-talk)))
-                (write-para (:body style-names) "")
+                (write-para paragraph-style "")
                 (write-heading (:h3 heading-formats) (str "Speaker: " (:speaker ted-talk)))
-                (write-para (:body style-names) "")
+                (write-para paragraph-style "")
                 (write-heading (:h3 heading-formats) "Introduction")
-                (write-para (:body style-names) (:description ted-talk))
-                (write-para (:body style-names) "")
+                (write-para paragraph-style (:description ted-talk))
+                (write-para paragraph-style "")
                 (write-heading (:h3 heading-formats) "Transcript"))]
     (reduce (fn [d para]
-              (write-para d (:body style-names) (str (:timestamp para) "\n" (:content para)))
-              (write-para d (:body style-names) ""))
+              (write-para d paragraph-style (str (:timestamp para) "\n" (:content para)))
+              (write-para d paragraph-style ""))
             doc
             (:transcript ted-talk))))
 
