226e8cd9d97cfac1bec08d4eccc9bbdec5f47c3b
riwepo <riwepo.au@gmail.com>
2025-08-19 16:41:25 +1000

    move decode html here make entities local

--- src/ted_scrape/utils.clj
+++ src/ted_scrape/utils.clj
@@ -1,4 +1,17 @@
-(ns ted-scrape.utils)
+(ns ted-scrape.utils
+  (:require [clojure.string :as str]))
+
 (defn debug-print [val]
   (println "Debugging:" val)
   val)
+
+(defn decode-html [s]
+  (let [html-entities {"&apos;" "'"
+                       "&quot;" "\""
+                       "&amp;"  "&"
+                       "&lt;"   "<"
+                       "&gt;"   ">"}]
+    (reduce (fn [acc [k v]]
+              (str/replace acc k v))
+            s
+            html-entities)))
