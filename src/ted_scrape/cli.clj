52f4f381472528eed88a512b73d3986ccc80465c
riwepo <riwepo.au@gmail.com>
2025-08-20 09:24:41 +1000

    implemented command line checks

--- src/ted_scrape/cli.clj
+++ src/ted_scrape/cli.clj
@@ -1,21 +1,25 @@
 (ns ted-scrape.cli
   (:require [clojure.string :as str]
             [clojure.tools.cli :refer [parse-opts]]
+            [ted-scrape.utils :refer [valid-ted-transcript-url? valid-output-dir?]]
             [ted-scrape.core :refer [run]]))
 
 (def cli-options
   [["-u" "--url URL" "TED transcript URL"
-    :validate [#(re-matches #"^https://www\.ted\.com/talks/.+/transcript$" %)
-               "Must be a valid TED transcript URL"]]
+    :validate [valid-ted-transcript-url?
+               "Must be a valid TED transcript URL"]
+    :missing "URL is required"]
    ["-o" "--output-dir DIR" "Output directory"
-    :default "."]
+    :default "."
+    :validate [valid-output-dir?
+               "Must be a valid writable directory"]]
    ["-h" "--help"]])
 
 (defn usage [options-summary]
   (->> ["This program parses a TED talk video transcript from a given URL."
         "The transcript is saved to a LibreOffice .odt file."
         ""
-        "Usage: ted-scrape url [options]"
+        "Usage: ted-scrape [options]"
         ""
         "Options:"
         options-summary
@@ -33,29 +37,27 @@
   [args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
     (cond
-      (:help options) ; help => exit OK with usage summary
+      (:help options)                                       ; help => exit OK with usage summary
       {:exit-message (usage summary) :ok? true}
-      errors ; errors => exit with description of errors
+      errors                                                ; errors => exit with description of errors
       {:exit-message (error-msg errors)}
-      ;; custom validation on arguments
-      (and (= 1 (count arguments))
-           (#{"start" "stop" "status"} (first arguments)))
-      {:action (first arguments) :options options}
-      :else ; failed custom validation => exit with usage summary
-      {:exit-message (usage summary)})))
+      :else                                                 ; run the program with options
+      {:action "run" :options options})))
 
 (defn exit [status msg]
   (println msg)
-  (System/exit status))
+  (println status)) ; remove later
+;(System/exit status))
 
 (defn -main [& args]
-  (let [{:keys [url options exit-message ok?]} (validate-args args)]
+  (let [{:keys [options exit-message ok?]} (validate-args args)]
     (if exit-message
       (exit (if ok? 0 1) exit-message)
-      (run url options))))
+      (run options))))
 
 (comment
-  (-main )
+  ;(-main "--url" "https://www.ted.com/talks/slug/transcript")
+  (-main "-u" "https://www.ted.com/talks/slug/transcript" "-o" "C:/temp")
   (nop))
 
 
