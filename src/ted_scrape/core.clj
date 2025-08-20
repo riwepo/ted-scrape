bb5bd8272c7444d07f831319693cba2c504da20a
riwepo <riwepo.au@gmail.com>
2025-08-20 14:34:13 +1000

    parse out the ted talk

--- src/ted_scrape/core.clj
+++ src/ted_scrape/core.clj
@@ -1,6 +1,7 @@
 (ns ted-scrape.core
   (:require [clj-http.client :as http]
-            [ted-scrape.extract :refer [scrape-html-with-puppeteer-script html->hickory]]))
+            [ted-scrape.extract :refer [scrape-html-with-puppeteer-script html->hickory]]
+            [ted-scrape.parse :refer [parse-ted-talk]]))
 
 (defn reachable-url?
   "Returns true if the URL responds with a 2xx or 3xx status code."
@@ -17,13 +18,11 @@
     (not (reachable-url? url))
     {:status 1 :error "url not reachable"}
     (let [{:keys [exit out] :as result} (scrape-html-with-puppeteer-script url)]
-      ;(println exit result)
       (if (not (= 0 exit))
         result
-        (do
-          (println "hickory")
-          (html->hickory out "data/full-page-hickory.edn")
-          {:exit 0 :out "finished"})))))                    ; extra steps here
+        (let [hickory-tree (html->hickory out)
+              ted-talk (parse-ted-talk hickory-tree)]
+          {:exit 0 :out ted-talk})))))
 
 
 
