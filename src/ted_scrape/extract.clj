55619fe629da71da282741625abc21e257286f26
riwepo <riwepo.au@gmail.com>
2025-08-21 07:58:42 +1000

    revert uberjar resource file stuff, will use docker

--- src/ted_scrape/extract.clj
+++ src/ted_scrape/extract.clj
@@ -1,12 +1,8 @@
 (ns ted-scrape.extract
   (:require
-    [clojure.java.io :as io]
     [clojure.java.shell :refer [sh]]
     [hickory.core :as hickory]
-    [ted-scrape.utils :refer [decode-html]])
-  (:import [java.nio.file Files]
-           [java.nio.file.attribute FileAttribute]))
-
+    [ted-scrape.utils :refer [decode-html]]))
 
 (defn extract-tree [html]
   (-> html
@@ -21,31 +17,6 @@
   (let [result (sh "node" "resources/scripts/scrapeHtml.js" url)]
     ;(println result)
     result))
-
-(defn run-node-script [resource-path url]
-  (let [resource-url (io/resource resource-path)]
-    (cond
-      (nil? resource-url)
-      {:exit 1 :out "" :err (str "Resource not found: " resource-path)}
-
-      (.startsWith (.toString resource-url) "file:")
-      ;; Run directly from file system
-      (let [file (io/file resource-url)]
-        (sh "node" (.getAbsolutePath file) url))
-
-      (.startsWith (.toString resource-url) "jar:")
-      ;; Extract to temp file
-      (let [temp-file (Files/createTempFile "scrapeHtml" ".js" (make-array FileAttribute 0))
-            temp-path (.toFile temp-file)]
-        (with-open [in (io/input-stream resource-url)
-                    out (io/output-stream temp-path)]
-          (io/copy in out))
-        (let [result (sh "node" (.getAbsolutePath temp-path) url)]
-          (.delete temp-path)
-          result)))))
-
-
-
 
 (defn decode-text-nodes [node]
   (cond
