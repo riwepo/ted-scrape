e85d9b5d23f5e653151a9fd90667e25998ee67d4
riwepo <riwepo.au@gmail.com>
2025-08-23 08:07:03 +1000

    debug print not used moved validators to cli namespace

--- src/ted_scrape/utils.clj
+++ src/ted_scrape/utils.clj
@@ -2,10 +2,6 @@
   (:require [clojure.string :as str])
   (:import (java.io File)))
 
-(defn debug-print [val]
-  (println "Debugging:" val)
-  val)
-
 (defn decode-html [s]
   (let [html-entities {"&apos;" "'"
                        "&quot;" "\""
@@ -16,19 +12,6 @@
               (str/replace acc k v))
             s
             html-entities)))
-
-(defn valid-ted-transcript-url? [url]
-  (let [pattern #"^https://www\.ted\.com/talks/([a-z0-9_\-]+)/transcript$"
-        match (re-matches pattern url)]
-    (boolean match)))
-
-(defn valid-output-dir?
-  "Returns true if the path exists, is a directory, and is writable."
-  [^String path]
-  (let [f (File. path)]
-    (and (.exists f)
-         (.isDirectory f)
-         (.canWrite f))))
 
 (defn bind [result f]
   "helper function for monadic chaining"
