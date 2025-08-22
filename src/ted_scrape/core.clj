c4bee069b404f94d13a02759a01280350c619ebe
riwepo <riwepo.au@gmail.com>
2025-08-23 06:20:42 +1000

    if not

--- src/ted_scrape/core.clj
+++ src/ted_scrape/core.clj
@@ -24,7 +24,7 @@
       {:status 1 :error "url not reachable"})
     (let [{:keys [exit out] :as result} (scrape-html-with-puppeteer-script url)]
       (println "scrape-html-with-puppeteer-script returned exit status " exit)
-      (if (not (= 0 exit))
+      (if-not (= 0 exit)
         result
         (let [hickory-tree (html->hickory out)
               ted-talk (parse-ted-talk hickory-tree)
