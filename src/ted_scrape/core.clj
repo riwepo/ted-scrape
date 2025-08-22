acf671579eeef1e6854f68e7fe8e359555b8ba6b
riwepo <riwepo.au@gmail.com>
2025-08-23 07:48:53 +1000

    re-factor using bind

--- src/ted_scrape/core.clj
+++ src/ted_scrape/core.clj
@@ -1,37 +1,24 @@
 (ns ted-scrape.core
-  (:require [clj-http.client :as http]
-            [clojure.string :as str]
-            [ted-scrape.extract :refer [scrape-html-with-puppeteer-script html->hickory]]
+  (:require [ted-scrape.extract :refer [scrape->hickory]]
             [ted-scrape.parse :refer [parse-ted-talk]]
-            [ted-scrape.odt-writer :refer [write-ted-talk]]
-            [clojure.java.io :as io]))
-
-(defn reachable-url?
-  "Returns true if the URL responds with a 2xx or 3xx status code."
-  [^String url]
-  (try
-    (let [resp (http/head url {:throw-exceptions false})]
-      (<= 200 (:status resp) 399))
-    (catch Exception _ false)))
+            [ted-scrape.odt-writer :refer [save-ted-talk]]
+            [ted-scrape.utils :refer [bind]]))
 
 
-(defn run [{:keys [url output-dir]}]
-  (println "running ted-scrape.core.run with args: " url output-dir)
-  (if
-    (not (reachable-url? url))
-    (do
-      (println "url not reachable")
-      {:status 1 :error "url not reachable"})
-    (let [{:keys [exit out] :as result} (scrape-html-with-puppeteer-script url)]
-      (println "scrape-html-with-puppeteer-script returned exit status " exit)
-      (if-not (= 0 exit)
-        result
-        (let [hickory-tree (html->hickory out)
-              ted-talk (parse-ted-talk hickory-tree)
-              title (str/replace (:title ted-talk) #" " "-")
-              doc (write-ted-talk ted-talk)]
-          (.save doc (io/file output-dir (str title ".odt")))
-          {:exit 0})))))
+(defn process-ted-talk [output-dir url]
+  (-> (scrape->hickory url)
+      (bind parse-ted-talk)
+      (bind (partial save-ted-talk output-dir))))
+
+(comment
+  (bind (scrape->hickory "https://www.ted.com/talks/eric_schmidt_the_ai_revolution_is_underhyped/transcript") parse-ted-talk)
+  (bind {:ok? true :value "data/sample-page.edn"} parse-ted-talk)
+  (process-ted-talk
+    "data"
+    "https://www.ted.com/talks/eric_schmidt_the_ai_revolution_is_underhyped/transcript")
+  nil)
+
+
 
 
 
